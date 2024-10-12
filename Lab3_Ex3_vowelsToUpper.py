3988aedf2f352e330f508e38258161c4e84b65c7
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-12 23:22:24 +0800

    Create class

--- Lab3_Ex3_vowelsToUpper.py
+++ Lab3_Ex3_vowelsToUpper.py
@@ -21,6 +21,11 @@
 
 
 # Create class
+class vowel_converter:
+    def __init__(self):
+        pass
 # Convert vowels to uppercase
+    def vowels_to_upper(self, input_string):
+        return ''.join([char.upper() if char in 'aeiou' else char for char in input_string])
 # Input strings
 # Print results
