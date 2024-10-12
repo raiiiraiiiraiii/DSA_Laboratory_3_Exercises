3f708a38d261c5adac6d0228b17eb27f2a2dcd56
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-12 23:18:22 +0800

    Added Pseudocode

---
+++ Lab3_Ex3_vowelsToUpper.py
@@ -0,0 +1,26 @@
+'''
+    ### Provide a list comprehension that implementation for a function called vowelsToUpper with the following signature:
+
+    method name : vowelsToUpper
+
+    input argument : String 
+
+    return argument : String
+
+    vowelsToUpper must return a version of its String argument with all its vowels changed to their uppercase forms. Nonvowel characters stay as is.
+
+    ### Sample Calls
+
+    vowelsToUpper "" == ""
+
+    vowelsToUpper "Hello, world!" == "HEllO, wOrld!"
+
+    vowelsToUpper "hello hi bye" == "hEllO hI byE"
+
+'''
+
+
+# Create class
+# Convert vowels to uppercase
+# Input strings
+# Print results
