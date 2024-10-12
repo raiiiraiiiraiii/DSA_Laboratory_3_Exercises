03ebb9a54015bbf62a221b9c40ed313f7db34ea0
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-12 22:11:49 +0800

    Added Pseudocode (1)

---
+++ Lab3_Ex1_Square.py
@@ -0,0 +1,15 @@
+
+''' 
+
+from a given list of integers, create a new list using comprehension that will compute the square of odd integer elements.
+
+sample calls
+
+[2,4,3] == [9]
+[0,0,1,1] == [1,1] 
+
+'''
+
+# Create class
+# Search for the odd elements in the list/s and square them
+# Display the result
