ec26f25ff3a20013d5cba374e24606b018873a9a
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-12 22:53:58 +0800

    Create class

--- Lab3_Ex2_List_of_Tuples.py
+++ Lab3_Ex2_List_of_Tuples.py
@@ -14,6 +14,11 @@ result: listOfTuple[(1,"mark"),(2,"alice"),(3,"john")]
 '''
 
 # Create class
+class list_of_tuple_generator:
+    def __init__(self, numbers, names):
+        self.numbers = numbers
+        self.names = names
+
 # Generate tuples
 # Lists
 # Generate and print the list of tuples
