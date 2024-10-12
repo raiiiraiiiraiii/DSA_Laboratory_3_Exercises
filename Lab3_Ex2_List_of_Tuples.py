3f9839d094624dbb640bc26341e09af986bf5911
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-10-12 22:51:33 +0800

    Added Pseudocode

---
+++ Lab3_Ex2_List_of_Tuples.py
@@ -0,0 +1,19 @@
+'''
+
+## Lab exercise 2
+
+Using comprehension,create a new list of tuples from two given lists:
+
+### sample calls
+
+list1[1,2,3]
+list2["mark","alice","john]
+
+result: listOfTuple[(1,"mark"),(2,"alice"),(3,"john")]
+
+'''
+
+# Create class
+# Generate tuples
+# Lists
+# Generate and print the list of tuples
